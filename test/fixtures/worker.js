376431f723be32cc725ec58d6e87f59d591d31cf
Malte Ubl <malubl@joose.intern>
2010-03-14 17:13:16 +0100

    Better testing infrastructure and more tests including test for working in parallel and a test for a worker with a syntax error.

--- test/fixtures/worker.js
+++ test/fixtures/worker.js
@@ -2,7 +2,7 @@
 var path = require("path");
  
 exports.testDir = path.dirname(__filename);
-exports.libDir = path.join(exports.testDir, "../../../lib");
+exports.libDir = path.join(exports.testDir, "../../lib");
  
 require.paths.unshift(exports.libDir);
  
@@ -10,13 +10,6 @@ var sys    = require("sys");
 var worker = require("worker").worker;
  
 worker.onmessage = function (msg) {
-    sys.error("OnMessage " +JSON.stringify(msg));
-    
-    if(msg.fib) {
-      worker.postMessage(fib(msg.fib*1));
-      return;
-    }
-    
     if(msg.wait) {
       setTimeout(function () {
         worker.postMessage("Waited")
@@ -33,8 +26,3 @@ worker.onmessage = function (msg) {
       worker.postMessage(msg);
     }, 100 * msg.output)
 };
- 
- 
-function fib(n) {
-  return n < 2 ? n : fib(n-1)+fib(n-2);
-}
--- test/test-worker.js
+++ test/test-worker.js
@@ -5,11 +5,12 @@ var Worker = require("../lib/worker").Worker;
  
 process.ENV["NODE_PATH"] = process.libDir;
  
-function makeWorker () {
-  return new Worker(__filename.replace("test-", "fixtures/"));
+function makeWorker (filename) {
+  return new Worker(__dirname+"/fixtures/"+filename);
 }
- 
-var worker = makeWorker();
+
+// basic test
+var worker = makeWorker("worker.js");
  
 worker.onmessage = function (msg) {
   if (msg.input) {
@@ -32,43 +33,78 @@ worker.postMessage({
 worker.postMessage({
   input: 4
 });
- 
+
+// error handling
 setTimeout(function () {
   setTimeout(function () {
-    var w2 = makeWorker();
+    
+    var w2 = makeWorker("worker.js");
     w2.postMessage({
       error: true
     });
     w2.addListener("error", function () {
-      assert.ok(true, "Received expected error");
+      assert.ok(true, "Received expected error via event");
       w2.terminate();
     });
     w2.addListener("message", function () {
       assert.ok(false, "Wanted an error, but got a message");
       w2.terminate();
     });
+    
+    var w3 = makeWorker("worker.js");
+    w3.postMessage({
+      error: true
+    });
+    w3.onerror = function () {
+      assert.ok(true, "Received expected error with onerror");
+      w3.terminate();
+    };
+    w3.addListener("message", function () {
+      assert.ok(false, "Wanted an error, but got a message");
+      w3.terminate();
+    });
+    
   }, 10);
 }, 10);
 
+// syntax error handling
+var syntaxError = makeWorker("syntax-error-worker.js");
+var hadSyntaxError = false;
+syntaxError.onerror = function (err) {
+  if(err instanceof SyntaxError) {
+    hadSyntaxError = true;
+  }
+}
+process.addListener("exit", function () {
+  assert.ok(hadSyntaxError, "detected syntax error");
+})
 
-var fibWorker = makeWorker();
+// long running processes
+var fibWorker = makeWorker("fib-worker.js");
 sys.puts("Giving hard work to worker")
 fibWorker.postMessage({
   fib: 41
 });
-var ret = false;
+var longRunningReturned = false;
+var interval = setInterval(function () {
+  sys.puts("# working in the background")
+}, 500);
 fibWorker.addListener("message", function (fib) {
   sys.error("Fib "+fib);
-  ret = true;
+  longRunningReturned = true;
   assert.ok(fib === 165580141, "Worker can do long running stuff.")
   fibWorker.terminate();
+  clearInterval(interval);
 });
 for(var i = 0; i < 100; ++i) {
   // counting;
 }
-assert.ok(!ret, "Can do work while background spins");
- 
-var waitWorker = makeWorker();
+assert.ok(!longRunningReturned, "Can do work while background spins");
+process.addListener("exit", function () {
+  assert.ok(longRunningReturned, "Did long running calculation")
+})
+
+var waitWorker = makeWorker("worker.js");
 waitWorker.postMessage({
   wait: true
 });
